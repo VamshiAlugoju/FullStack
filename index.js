1405bcd75b15685d681eb1d152e6f28890d0f94b
vamshi <alugojuvamshi@gmail.com>
2023-04-28 16:21:03 +0530

    login added

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-function validate(e)
+function signUp(e)
 {
     e.preventDefault();
     let vals = e.target;
@@ -8,12 +8,35 @@ function validate(e)
         email: vals.Email.value,
         password: vals.Password.value
     }
-      
-  axios.post("wwww.localhost:3000/users/Signup",payLoad)
+    //   console.log(payLoad)
+  axios.post("http://localhost:3000/users/Signup",payLoad)
   .then(res=>{
     console.log(res);
+    window.location.pathname("./login.html")
   })
   .catch(err=>{
-    console.log(err);
+    alert(err.response.data.message);
+    // console.log(err.response.data)
   })
+}
+
+async function login(e)
+{
+    e.preventDefault();
+    let vals = e.target;
+    let payLoad = {
+        email: vals.Email.value,
+        password: vals.Password.value
+    };
+
+    try{
+      
+       let result = await axios.post("http://localhost:3000/users/Login",payLoad);
+        // alert(result.response )
+        alert(result.data.message)
+    }
+    catch(err){
+        // alert(err.response)
+         alert(err.response.data.message)
+    }
 }
