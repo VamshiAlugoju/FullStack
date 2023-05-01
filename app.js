76fb56c94c2d25aab376e87e609b0d5ae87eb46c
vamshi <alugojuvamshi@gmail.com>
2023-05-01 16:34:05 +0530

    forgot passowrd added

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const bodyparser = require("body-parser");
 const cors = require("cors");
 const Sequelize = require("sequelize")
 const sequelize = require("./util/database")
+const path = require("path");
 
 const User = require("./models/user");
 const Expenses = require("./models/expense");
 const Orders = require("./models/order");
 const LeaderBoard = require("./models/leaderBoard");
+const ForgotRequests = require("./models/ForgotRequests");
 
 const userauthentication = require("./middleware/Authenticate");
 
@@ -19,17 +21,21 @@ const passwordRoutes = require("./routes/password");
 
 app.use(bodyparser.json({extended :false}))
 app.use(cors());
+app.use(express.static(path.join(__dirname,"public")))
  
 User.hasMany(Expenses);
 Expenses.belongsTo(User);
 User.hasMany(Orders);
 Orders.belongsTo(User);
+User.hasMany(ForgotRequests);
+ForgotRequests.belongsTo(User);
 
 
 app.use("/users",userRoutes)
 app.use("/Expenses",userauthentication.Authenticate,expenseRoutes)
 app.use("/purchase",premimuRoutes);
 app.use("/password",passwordRoutes);
+
 
 sequelize.sync()
 .then(result=>{
--- routes/password.js
+++ routes/password.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const passwordControllers = require("../controllers/passwordControllers");
 
 router.post("/forgotpassword",passwordControllers.forgotPassword);
+router.get("/resetPassword/:id",passwordControllers.resetPassword)
+router.post("/changePassword",passwordControllers.changePassword);
 
 
 module.exports = router;
